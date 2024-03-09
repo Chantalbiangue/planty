f401d510dfe35070374c2c916f13a801f67c4095
Chantal Missamou <biangue@yahoo.com>
2024-03-09 18:03:35 +0100

    site banner done

---
+++ wp-content/themes/kadence-child/page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The main single item template file.
+ *
+ * @package kadence
+ */
+
+namespace Kadence;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+
+
+the_content();
+
+get_footer();
